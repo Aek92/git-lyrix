49b97509a9809502eb210bc4bd3c4b59a8cf3109
Mikkel Eriksen Klokkerud <mikkelek93@gmail.com>
2020-03-24 16:37:53 +0100

    adding text file for required functionality and deletes js content from main.js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 let url = "https://api.datamuse.com/words?";
-let queryRhyme = "rel_rhy=";
-let queryRhyme = "rel_syn=";
 
-const Hooks = {
+
+
+// QUERY STRINGS
+let queryRhyme = "rel_rhy=";
+let queryApproxRhyme = "rel_nry=";
+let queryAdjFromNoun = "rel_jjb=";
+let queryNounFromAdj = "rel_jja=";
+
+let domStrings = () => {
     
+
 }
