62daf2345b885f6ad1525e2c540b630418ff8f8e
Aek92 <aek92.ak@gmail.com>
2020-03-27 03:20:24 +0100

    a

--- main.js
+++ main.js
@@ -1,98 +1,85 @@
 // Lists relevant information from API provider like url and query strings
 // Change values and add/remove new properties to get alter functionality
+
+
+
+let wordArray = [ 'stupid' ];
+
 const datamuse = {
-    url: "https://api.datamuse.com/words?",
-    queryStrings: {
-        rhyme: "rel_rhy=",
-        approxRhyme: "rel_nry=",
-        ajective: "rel_jjb=",
-        noun: "rel_jja=",
-        synonym: "rel_syn=",
-    }
+	url: 'https://api.datamuse.com/words?',
+	queryStrings: {
+		rhyme: 'rel_rhy=',
+		approxRhyme: 'rel_nry=',
+		adjective: 'rel_jjb=',
+		noun: 'rel_jja=',
+		synonym: 'rel_syn='
+	}
+};
+
+function addSection (){
+    var html, newHtml, element;
+    
+    element = document.getElementById('container');
+	// Create HTML string with placeholder text
+	html =
+		'<div id="sectionContainer"><div id="songSections"><div class="section" id="section-%id%"><input class="labelInput" id="input-%id%" value="%type%"></input><div class="textBackground" id="textBackground1"><textarea readonly id="text-%id%" class="textarea" cols="50" rows="4"></textarea></div></div></div></div>';
+    html.trim();
+
+    // Iterates object and replaces the placeholder text
+    for (let [key, value] of Object.entries(datamuse.queryStrings)) {
+        let sub = `${value}`.substring(4,7);
+        newHtml = html.split('%id%').join(sub);
+        newHtml = newHtml.split('%type%').join(key);
+        element.insertAdjacentHTML('beforeend', newHtml);
+      }
 }
 
+document.getElementById('addSectionBtn').addEventListener('click', () => {
+	// 1. Create new html section with unique ID
+	addSection();
+	// 2. Request rhyme, synonym, noun etc....(param)
+	genQueryString();
 
-// Lists all class names for section elements in order.
-// IDs are assigned as classname + sectionNumber.
-class sectionModuleTemplate {
-    constructor() {
-        section = "section";
-        labelInput = "labelInput",
-        textBackground = "textBackground",
-        textarea = "textarea";
-        text = "text";
-        hint = "hint";
-
-    }
-}
-
-
-// Introduces "Add section" functionality
-// Temporarily tied to the HTML button 
-const addSectionBtn = document.getElementById("addSectionBtn");
-let sectionsCreated = 1;
-let sectionName = "section" + sectionsCreated;
-let sections = [];
-
-addSectionBtn.addEventListener("click", function () {
-    sectionsCreated++;
-    window["section" + sectionsCreated] = {};
-    for (let key in sectionModuleTemplate) {
-        window["section" + sectionsCreated][key] = sectionModuleTemplate[key] + sectionsCreated;
-    }
-    sections.push(window["section" + sectionsCreated]);
-    console.log(sections);
+    // 3. Display request in html section
+        // Display() function handles this
 });
 
+console.log(datamuse.queryStrings);
+// Maps url + type of req + word
+const genQueryString = function (){
+	wordArray.forEach((el) => {
+		let urlArr = [];
+		Object.keys(datamuse.queryStrings).map((key, value) => {
+			urlArr.push(datamuse.url + datamuse.queryStrings[key] + el);
+		});
+		fetchData(urlArr);
+	});
+};
 
-
-// wordArray: Simulating the words array made from textarea userinput:
-// result: Simulating the location in a section object where arrays of "word-objects" will be pushed.
-let wordArray = ["cure", "feel", "they", "run", "see", "drive", "green", "wall", "blue", "win"];
-let result = [];
-
-function requestSuggestions(wordsArr) {
-    wordsArr.forEach(word => {
-        for (queryString in datamuse.queryStrings) {
-            let fetchIt = async () => {
-                let endpoint = datamuse.url + datamuse.queryStrings[queryString] + word;
-                await fetch(endpoint)
-                .then(response => { return response.json(); })
-                .then(data => { return result.push(data.splice(0, 10)); })
-                .catch(error => {return console.log("This error was beautifully caught by Mikkel Inc.: "+ error)});
-            }
-            fetchIt(word, queryString);
-        }
-    });
+//Fetch data ------- >
+function fetchData (urls){
+	return Promise.all(
+		urls.map((url) =>
+			fetch(url)
+				.then((file) => file.json())
+				.then((data) => {
+                    let result = []
+                    console.log('------------------')
+                    data.some((el, index) => {
+                        result.push(el.word);
+                        //console.log(el.word + '\t\t\t' + url);
+                        return ++index === 10
+                    })
+                    display(result, url.substring(35, 38));
+                })
+				.catch((error) => { console.log("error", url )})
+		)
+	);
 }
-requestSuggestions(wordArray);
 
-
-// Test-run fetch and calculate response time
-// Set variable "time" to ms allowance
-let time = 170;
-
-setTimeout(function() {
-    
-    let numberOfWords = wordArray.length;
-    let numberOfQueryStrings = Object.keys(datamuse.queryStrings).length;
-    let numberOfRequests = numberOfWords * numberOfQueryStrings;
-    let suggestionCount = 0;
-    result.forEach(element => {
-        suggestionCount += element.length;
-    });
-    console.log(result);
-    console.log(`====== Fetch Test Results ======`);
-    console.log(`Requests attempted: ${numberOfRequests}`);
-    console.log(`Time allowed: ${time}ms`);
-    console.log(`Successful requests: ${result.length}`);
-    console.log("Word-suggestions saved: " + suggestionCount);
-    console.log(`------ Speed Results ------`);
-    console.log(`Avg time to complete a request: ${Math.round(time / result.length)}ms`);
-    console.log(`Estimated time needed to complete all requests: ${Math.round(numberOfRequests * (time / result.length))}ms`);
-    if (result.length < 15) {
-        console.log("WARNING: inaccurate results - INCREASE time allowance.");
-    } else if (result.length === 50) {
-        console.log("WARNING: inaccurate results - DECREASE time allowance.");
-    }
-}, time);
+// Displays data ---> Needs improvement
+let display = (data, type) => {
+    // Gets Html Element based on type av request
+    let textArea = document.getElementById(`text-${type}`);
+    textArea.value = data.join('\t');
+};
